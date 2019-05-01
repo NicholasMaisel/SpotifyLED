e5cbcd34ae380e7e27cbfdcb7fb653fa11b6a6d6
NicholasMaisel <NIcholasMaisel@outlook.com>
2019-05-01 13:55:03 -0400

    Create nba_data_filter.py

---
+++ nba_data_filter.py
@@ -0,0 +1,29 @@
+import csv
+nba_data = []
+mod_data = []
+with open('playerdata.txt','r') as file:
+    data = csv.DictReader(file)
+    [nba_data.append(x) for x in data]
+
+for player in nba_data:
+    if player['Player'] in [x['Player'] for x in mod_data]:
+        pass
+    else:
+        mod_data.append(player)
+
+#[print(x['Player'],',', x['Tm']) for x in mod_data]
+with open('moddata.txt','w') as file:
+    stats = mod_data[0].keys()
+    for stat in stats:
+        if stat == next(reversed(stats)):
+            print(stat)
+        else:
+            print(stat+',', end = '')
+            
+    for player in mod_data:
+        for stat in stats:
+            if stat == next(reversed(stats)):
+                print(player[stat])
+            else:
+                print(player[stat]+',', end = '')
+
