d65e8b20eb8073de788d1c66e7b6ed3cd40712fa
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-05-21 18:03:09 -0400

    adds some info

--- config.py
+++ config.py
@@ -2,3 +2,20 @@
 client_id = '5586d8016f314082ac91e3cf19f7d6ad'
 username = 'nick9875'
 client_secret = 'bac8e11d927d4ddf8427ff202fdec94d'
+scope = 'playlist-modify-public,user-read-playback-state,streaming'
+bluetooth_mac = '3c:a3:08:be:e1:ff'
+
+default_track_uri = '1yTJYoSpt2E5h9QltN1ndw'
+
+led_colors = [[b'\x45',b'\x62',b'\x45'],
+            [b'\x55',b'\x45',b'\x55'],
+            [b'\x62',b'\x45',b'\x45'],
+            [b'\x62',b'\x45',b'\x62'],
+            [b'\x55',b'\x45',b'\x45'],
+            [b'\x62',b'\x62',b'\x45'],
+            [b'\x45',b'\x45',b'\x62'],
+            [b'\x50',b'\x60',b'\x70'],
+            [b'\xfa',b'\x55',b'\x72'],
+            [b'\x45',b'\x62',b'\x62'],
+            [b'\x35',b'\x60',b'\x42'],
+            [b'\x42',b'\x22',b'\x79']]
