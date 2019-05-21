cf0432e749db1008df77863e31c845a19b417002
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-05-21 17:57:13 -0400

    adds file

---
+++ bluetoothhandler.py
@@ -0,0 +1,10 @@
+import bluepy
+from config import *
+
+class LEDController():
+    def __init__(self):
+        self.peripheral = bluepy.btle.Peripheral(bluetooth_mac)
+        self.chars = [x for x in perif.getDescriptors()]
+        self.red = chars[36]
+        self.green = chars[39]
+        self.blue = chars[42]
