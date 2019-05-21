462df44f029ab496f20a27b392853c2614c8dc10
Nicholas.Maisel <Nicholasmaisel@outlook.com>
2019-05-21 18:06:11 -0400

    fixes invalid reference

--- bluetoothhandler.py
+++ bluetoothhandler.py
@@ -4,7 +4,7 @@ from config import *
 class LEDController():
     def __init__(self):
         self.peripheral = bluepy.btle.Peripheral(bluetooth_mac)
-        self.chars = [x for x in perif.getDescriptors()]
+        self.chars = [x for x in self.peripheral.getDescriptors()]
         self.red = chars[36]
         self.green = chars[39]
         self.blue = chars[42]
