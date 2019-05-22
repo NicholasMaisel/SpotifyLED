1ac01a09647d1d1c75a1ad7a82c6af6e327abe56
NicholasMaisel <NIcholasMaisel@outlook.com>
2019-05-22 14:23:19 -0400

    Update colorhandler.py

--- colorhandler.py
+++ colorhandler.py
@@ -1,9 +1,9 @@
 from config import *
 
 def rgb_to_hex():
-    color_list = color_list
-    for note_index in range(len(color_list)):
-        for color_index in range(len(color_list[note_index])):
-            color_setting = color_list[note_index][color_index]
-            color_list[note_index][color_index] = "{0:x}".format(color_setting)
-    return(color_list)
+    colors = color_list
+    for note_index in range(len(colors)):
+        for color_index in range(len(colors[note_index])):
+            color_setting = colors[note_index][color_index]
+            colors[note_index][color_index] = "{0:x}".format(color_setting)
+    return(colors)
